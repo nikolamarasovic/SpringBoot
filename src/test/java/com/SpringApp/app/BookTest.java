586ad95bd9779f51eafbbe0dae0a3484fb88e929
Nikola Marasovic <nikola.marasovic@ericsson.com>
2020-08-15 20:58:23 +0200

    new test case

---
+++ src/test/java/com/SpringApp/app/BookTest.java
@@ -0,0 +1,25 @@
+package com.SpringApp.app;
+
+import static org.junit.Assert.assertEquals;
+import com.SpringApp.app.ViewModels.BookViewModel;
+import com.SpringApp.app.domain.Book;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class BookTest {
+ Book book;
+ BookViewModel book1;
+ 
+ @Before
+ public void setUp(){
+    book = new Book();
+    
+ }
+ @Test
+ public void getId() throws Exception{
+    book.setId(3);
+    book1 = new BookViewModel(book);
+    assertEquals(book.getId(),book1.getId());
+ }
+}
